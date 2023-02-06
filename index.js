86ce8cb1e5331a2103eee3878df9fd92fd79222d
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-06 17:32:09 +0100

    feat: added core library business logic

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+const { getJsonModel } = require('./utils.js')
+const genericMachineV1 = getJsonModel('v1-generic-machine')
+
+module.exports = {
+  genericMachine: {
+    v1: genericMachineV1
+  }
+}
