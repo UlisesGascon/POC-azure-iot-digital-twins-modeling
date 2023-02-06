3b2ac48fe2462683dd1e66fdc84546a65e41aef5
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-06 17:41:40 +0100

    chore: linting

--- utils.js
+++ utils.js
@@ -4,5 +4,5 @@ const { join } = require('path')
 const getJsonModel = (modelFileName) => readFileSync(join(process.cwd(), `./models/${modelFileName}.json`), 'utf8')
 
 module.exports = {
-    getJsonModel
+  getJsonModel
 }
